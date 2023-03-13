f63f0a0427aff1058fb2600e0321103160a7f284
bojuns <86217481+bojuns@users.noreply.github.com>
2023-03-13 17:43:22 -0400

    Working with interrupts, slight race condition

--- KeyPeripheral.ino
+++ KeyPeripheral.ino
@@ -1,25 +1,22 @@
 #include <ArduinoBLE.h>
 
 #define UUID "19B10010-E8F2-537E-4F6C-D104768A1214"
-#define DEBUG true
+#define DEBUG false
 
 const int keyPin = D1;
 BLEService keyService(UUID);  // create service
 // create button characteristic and allow remote device to get notifications
 BLEByteCharacteristic keyCharacteristic(UUID, BLERead | BLENotify);
-volatile bool flag;
+volatile bool keyEventHappened;
 
 void setup() {
   if (DEBUG) {
     Serial.begin(9600);
-    while (!Serial)
-      ;
+    while (!Serial);
   }
 
-  pinMode(keyPin, INPUT_PULLDOWN);  // use button pin as an input
-  flag = false;
-  attachInterrupt(keyPin, keyPressHandler, CHANGE);
-
+  pinMode(keyPin, INPUT);  // use button pin as an input
+  
   // begin initialization
   if (!BLE.begin()) {
     if (DEBUG) Serial.println("starting Bluetooth® Low Energy module failed!");
@@ -43,37 +40,33 @@ void setup() {
 
   // start advertising
   BLE.advertise();
-
+  
   if (DEBUG) Serial.println("Bluetooth® device active, waiting for connections...");
+  
+  // Interrupt with key press
+  attachInterrupt(keyPin, keyPressHandler, CHANGE);
+  keyEventHappened = false;
 }
 
 void loop() {
-  // poll for Bluetooth® Low Energy events
-  BLE.poll();
-  if (flag) {
-    Serial.println("Flag true");
-    flag = false;
+  BLEDevice receiver = BLE.central();
+  if (receiver) {
+    while (receiver.connected()) {
+      delay(100);
+    }
+    if (DEBUG) Serial.println("Disconnected");
   }
 }
 
 void keyPressHandler() {
-  flag = true;
-  // // read the current button pin state
-  // char keyValue = digitalRead(keyPin);
+  // read the current button pin state
+  char keyValue = digitalRead(keyPin);
+  
+  // has the value changed since the last read
+  bool keyChanged = keyCharacteristic.value() != keyValue;
 
-  // // has the value changed since the last read
-  // bool keyChanged = keyCharacteristic.value() != keyValue;
-
-  // if (keyChanged) {
-  //   // Key state changed, update characteristic
-  //   keyCharacteristic.writeValue(keyValue);
-  // }
-
-  // if (DEBUG && (keyCharacteristic.written() || keyChanged)) {
-  //   if (keyCharacteristic.value()) {
-  //     Serial.println("key pressed");
-  //   } else {
-  //     Serial.println("key released");
-  //   }
-  // }
+  if (keyChanged) {
+    // Key state changed, update characteristic
+    keyCharacteristic.writeValue(keyValue);
+  }
 }
