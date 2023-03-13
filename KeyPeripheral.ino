262863970f7bc136eff4607be720b3aa7fe7dd9e
bojuns <86217481+bojuns@users.noreply.github.com>
2023-03-13 18:37:21 -0400

    Optimization without interrupts

--- KeyPeripheral.ino
+++ KeyPeripheral.ino
@@ -4,29 +4,27 @@
 #define DEBUG false
 
 const int keyPin = D1;
-BLEService keyService(UUID);  // create service
+BLEService keyService(UUID); // create service
 // create button characteristic and allow remote device to get notifications
 BLEByteCharacteristic keyCharacteristic(UUID, BLERead | BLENotify);
-volatile bool keyEventHappened;
 
 void setup() {
   if (DEBUG) {
     Serial.begin(9600);
     while (!Serial);
   }
-
-  pinMode(keyPin, INPUT);  // use button pin as an input
   
+  pinMode(keyPin, INPUT_PULLUP); // use button pin as an input
+
   // begin initialization
   if (!BLE.begin()) {
     if (DEBUG) Serial.println("starting Bluetooth® Low Energy module failed!");
-    while (1)
-      ;
+    while (1);
   }
 
   // set the local name peripheral advertises
   BLE.setLocalName("Key");
-
+  
   // set the UUID for the service this peripheral advertises:
   BLE.setAdvertisedService(keyService);
 
@@ -42,31 +40,31 @@ void setup() {
   BLE.advertise();
   
   if (DEBUG) Serial.println("Bluetooth® device active, waiting for connections...");
-  
-  // Interrupt with key press
-  attachInterrupt(keyPin, keyPressHandler, CHANGE);
-  keyEventHappened = false;
 }
 
 void loop() {
   BLEDevice receiver = BLE.central();
-  if (receiver) {
-    while (receiver.connected()) {
-      delay(100);
-    }
-    if (DEBUG) Serial.println("Disconnected");
-  }
-}
-
-void keyPressHandler() {
-  // read the current button pin state
-  char keyValue = digitalRead(keyPin);
   
-  // has the value changed since the last read
-  bool keyChanged = keyCharacteristic.value() != keyValue;
-
-  if (keyChanged) {
-    // Key state changed, update characteristic
-    keyCharacteristic.writeValue(keyValue);
+  if (receiver) {
+    BLE.stopAdvertise();
+    while (receiver.connected()) {
+      // read the current button pin state
+      char keyValue = digitalRead(keyPin);
+      
+      // has the value changed since the last read
+      bool keyChanged = keyCharacteristic.value() != keyValue;
+      
+      for (int i = 0; i < 10; i++) {
+        if (digitalRead(keyPin) != keyValue) {
+          keyChanged = false;
+          break;
+        }
+      }
+      if (keyChanged) {
+        // Key state changed, update characteristic
+        keyCharacteristic.writeValue(keyValue);
+      }
+    }
   }
+  BLE.advertise();
 }
